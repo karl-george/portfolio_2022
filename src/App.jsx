91e30f958943b3ef92841549a0a64ddc50b91dca
Karl George <karlgdev@gmail.com>
2022-10-18 13:12:42 +0200

    Working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <Navbar />
       <div className='container'>
-        <Navbar />
-        <Hero />
+        {/* <Hero />
         <Projects />
         <Skills />
         <Contact />
-        <Footer />
+        <Footer /> */}
       </div>
     </div>
   );
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,31 @@
 import './navbar.css';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header className='navbar'>
+      <div className='navbar__container container'>
+        <div className='navbar__brand'>
+          <h4>Karl George</h4>
+        </div>
+        <div className='navbar__links__container'>
+          <ul className='navbar__links'>
+            <li>
+              <a href=''>Home</a>
+            </li>
+            <li>
+              <a href=''>Projects</a>
+            </li>
+            <li>
+              <a href=''>Skills</a>
+            </li>
+            <li>
+              <a href=''>Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
