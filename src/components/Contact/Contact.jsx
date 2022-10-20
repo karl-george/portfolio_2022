28fc36f4340678f945ec38ece6731fd8a01e4534
Karl George <karlgdev@gmail.com>
2022-10-20 14:37:27 +0200

    Styling contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,11 +10,62 @@ const Contact = () => {
         </div>
         <div className='contact__form__container'>
           <div className='contact__form'>
-            <form action='#' method='POST'>
-              <input type='text' name='name' />
-              <input type='email' name='email' />
-              <input type='text' name='message' />
-              <button type='submit'>Send</button>
+            <form
+              action='https://getform.io/f/e3d7ce30-4a2a-4a1e-a881-335357e05e3a'
+              method='POST'
+            >
+              <div className='contact__form-field'>
+                <label
+                  className='contact__form-label
+                '
+                  htmlFor='name'
+                >
+                  Name
+                </label>
+                <input
+                  type='text'
+                  name='name'
+                  placeholder='Enter Your Name'
+                  className='contact__form-input'
+                />
+              </div>
+              <div className='contact__form-field'>
+                <label
+                  className='contact__form-label
+                '
+                  htmlFor='email'
+                >
+                  Email
+                </label>
+                <input
+                  type='text'
+                  name='email'
+                  placeholder='Enter Your Email'
+                  className='contact__form-input'
+                />
+              </div>
+              <div className='contact__form-field'>
+                <label
+                  className='contact__form-label
+                '
+                  htmlFor='message'
+                >
+                  Message
+                </label>
+                <textarea
+                  type='text'
+                  name='message'
+                  placeholder='Enter Your Message'
+                  className='contact__form-input'
+                  rows='7'
+                ></textarea>
+              </div>
+              <button
+                className='contact__form-button btn btn-filled'
+                type='submit'
+              >
+                Send
+              </button>
             </form>
           </div>
         </div>
