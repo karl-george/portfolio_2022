9c27fa4641a74d68597cd1b86a4e37bc9d5d50a1
Karl George <karlgdev@gmail.com>
2022-10-20 11:51:04 +0200

    Working on projects responsiveness

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,6 +1,7 @@
 import ProjectBlock from '../ProjectBlock/ProjectBlock';
 import news from '../../assets/news.png';
 import dga from '../../assets/dga.png';
+import weather from '../../assets/weather.png';
 import './projects.css';
 
 const Projects = () => {
@@ -19,14 +20,14 @@ const Projects = () => {
           codeLink={``}
         />
         <ProjectBlock
-          img={`https://picsum.photos/500/400`}
+          img={news}
           title={`Title of project`}
           content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
           liveLink={``}
           codeLink={``}
         />
         <ProjectBlock
-          img={`https://picsum.photos/500/400`}
+          img={weather}
           title={`Title of project`}
           content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
           liveLink={``}
