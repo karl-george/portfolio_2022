183bec700bd7e4ab5a187b0d6192e8f257595f75
Karl George <karlgdev@gmail.com>
2022-10-20 12:19:13 +0200

    Fixing a few styling errors and adding skills imgs

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,33 +6,35 @@ import './projects.css';
 
 const Projects = () => {
   return (
-    <section className='projects section__padding container'>
-      <div className='projects__heading'>
-        <h2 className='heading__title'>Projects</h2>
-        <div className='divider'></div>
-      </div>
-      <div className='projects__container'>
-        <ProjectBlock
-          img={dga}
-          title={`Title of project`}
-          content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
-          liveLink={``}
-          codeLink={``}
-        />
-        <ProjectBlock
-          img={news}
-          title={`Title of project`}
-          content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
-          liveLink={``}
-          codeLink={``}
-        />
-        <ProjectBlock
-          img={weather}
-          title={`Title of project`}
-          content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
-          liveLink={``}
-          codeLink={``}
-        />
+    <section className='projects section__padding'>
+      <div className='container'>
+        <div className='projects__heading'>
+          <h2 className='heading__title'>Projects</h2>
+          <div className='divider'></div>
+        </div>
+        <div className='projects__container'>
+          <ProjectBlock
+            img={dga}
+            title={`Title of project`}
+            content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
+            liveLink={``}
+            codeLink={``}
+          />
+          <ProjectBlock
+            img={news}
+            title={`Title of project`}
+            content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
+            liveLink={``}
+            codeLink={``}
+          />
+          <ProjectBlock
+            img={weather}
+            title={`Title of project`}
+            content={`Short explanation of what the project is about and how it was made. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut posuere felis. Aenean in mi vitae nisi pretium volutpat.`}
+            liveLink={``}
+            codeLink={``}
+          />
+        </div>
       </div>
     </section>
   );
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,7 +1,16 @@
 import './skills.css';
 
 const Skills = () => {
-  return <div>Skills</div>;
+  return (
+    <div className='skills section__padding'>
+      <div className='container'>
+        <div className='skills__heading'>
+          <h2 className='heading__title'>Skills</h2>
+          <div className='divider'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Skills;
