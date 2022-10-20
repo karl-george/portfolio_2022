4a1d031758ead30b64d775271ab67590651ef58f
Karl George <karlgdev@gmail.com>
2022-10-20 13:56:44 +0200

    Working on skills styling

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,3 +1,11 @@
+import reactImg from '../../assets/react.svg';
+import bootstrapImg from '../../assets/bootstrap.svg';
+import cssImg from '../../assets/css.svg';
+import gitImg from '../../assets/git.svg';
+import htmlImg from '../../assets/html.svg';
+import jsImg from '../../assets/js.svg';
+import nodeImg from '../../assets/node.svg';
+import pythonImg from '../../assets/python.svg';
 import './skills.css';
 
 const Skills = () => {
@@ -7,6 +15,40 @@ const Skills = () => {
         <div className='skills__heading'>
           <h2 className='heading__title'>Skills</h2>
           <div className='divider'></div>
+        </div>
+        <div className='skills__icons__container'>
+          <div>
+            <img src={reactImg} className='icon' />
+            <p>React</p>
+          </div>
+          <div>
+            <img src={jsImg} className='icon' />
+            <p>JavaScript</p>
+          </div>
+          <div>
+            <img src={htmlImg} className='icon' />
+            <p>HTML</p>
+          </div>
+          <div>
+            <img src={cssImg} className='icon' />
+            <p>CSS</p>
+          </div>
+          <div>
+            <img src={nodeImg} className='icon' />
+            <p>Node.js</p>
+          </div>
+          <div>
+            <img src={bootstrapImg} className='icon' />
+            <p>Bootstrap</p>
+          </div>
+          <div>
+            <img src={gitImg} className='icon' />
+            <p>GIT</p>
+          </div>
+          <div>
+            <img src={pythonImg} className='icon' />
+            <p>Python</p>
+          </div>
         </div>
       </div>
     </div>
