3b39d8f1a92e4a3e788ef968fddeb4c9de796b89
Karl George <karlgdev@gmail.com>
2022-10-31 12:13:23 +0100

    Fixing scroll bug on mobile

--- src/components/ProjectBlock/ProjectBlock.jsx
+++ src/components/ProjectBlock/ProjectBlock.jsx
@@ -4,10 +4,10 @@ const ProjectBlock = ({ img, title, content, liveLink, codeLink }) => {
   return (
     <div className='projectblock'>
       <div className='pb__container'>
-        <div className='pb__img' data-aos='fade-right' data-aos-once='true'>
+        <div className='pb__img' data-aos='fade-up' data-aos-once='true'>
           <img src={img} />
         </div>
-        <div className='pb__content' data-aos='fade-left' data-aos-once='true'>
+        <div className='pb__content' data-aos='fade-up' data-aos-once='true'>
           <h3 className='pb__content__title'>{title}</h3>
           <p className='pb__content__desc'>{content}</p>
           <a
@@ -27,5 +27,3 @@ const ProjectBlock = ({ img, title, content, liveLink, codeLink }) => {
 };
 
 export default ProjectBlock;
-
-//data-aos='fade-up' data-aos-once='true'
