2b72a08ad1e97d6318868c812e56c4cb735c230b
Karl George <karlgdev@gmail.com>
2023-01-09 15:00:29 +0100

    Adding picthat project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,5 @@
 import ProjectBlock from '../ProjectBlock/ProjectBlock';
-import news from '../../assets/news.png';
+import picthat from '../../assets/picthatn.png';
 import dga from '../../assets/dga.png';
 import weather from '../../assets/weather.png';
 import './projects.css';
@@ -14,18 +14,18 @@ const Projects = () => {
         </div>
         <div className='projects__container'>
           <ProjectBlock
+            img={picthat}
+            title={`PicThat Social Media`}
+            content={`A social media image sharing platform that allows users to sign in with their google account aswell as upload, favourite, download and search images and gifs uploaded by themselves and other users.`}
+            liveLink={`https://picthat.netlify.app/`}
+            codeLink={`https://github.com/karl-george/picthat`}
+          />
+          <ProjectBlock
             img={dga}
             title={`Digital Agency SPA`}
             content={`A website designed for a fictional design agency taken from initial design template through to finished product as a SPA.`}
             liveLink={`https://karl-george.github.io/digital-agency-landing/`}
             codeLink={`https://github.com/karl-george/digital-agency-landing`}
-          />
-          <ProjectBlock
-            img={news}
-            title={`News App`}
-            content={`A news application designed to allow the user to filter news stories based upon categories. The app uses NewsAPI to source news stories from all of the major news providers. Unfortunately the API does not allow deploying to a live site for free and so there is no live preview.`}
-            liveLink={``}
-            codeLink={`https://github.com/karl-george/news-app`}
           />
           <ProjectBlock
             img={weather}
